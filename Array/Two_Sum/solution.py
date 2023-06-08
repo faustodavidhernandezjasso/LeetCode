1f0161b9ca7564572b7db9d88bd0f797a332a443
Fausto David Hernández Jasso <fausto.david.hernandez.jasso@ciencias.unam.mx>
2023-06-07 22:15:55 -0600

    Solución óptima

--- Array/Two_Sum/solution.py
+++ Array/Two_Sum/solution.py
@@ -1,16 +1,26 @@
 class Solution:
 
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        indices = []
-        t = target
         length_of_list = len(nums)
+        dictionary = {}
         for i in range(length_of_list):
-            for j in range(i + 1, length_of_list):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            k = target - nums[i]
+            if (dictionary.get(k) == None):
+                dictionary[nums[i]] = i
+            else:
+                return [dictionary[k], i]
+        
+        
+        # indices = []
+        # t = target
+        # length_of_list = len(nums)
+        # for i in range(length_of_list):
+        #     for j in range(i + 1, length_of_list):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
 
 if __name__ == '__main__':
-    nums = [3,2,4]
+    nums = [3,3]
     target = 6
     s = Solution()
     print(s.twoSum(nums, target))
