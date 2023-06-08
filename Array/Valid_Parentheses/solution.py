b58b0f020b6867d305e043345aab9c5426de4747
Fausto David Hernández Jasso <fausto.david.hernandez.jasso@ciencias.unam.mx>
2023-06-08 00:37:58 -0600

    Quitando instrucciones print()

--- Array/Valid_Parentheses/solution.py
+++ Array/Valid_Parentheses/solution.py
@@ -1,23 +1,24 @@
 class Solution:
 
+    # def isValid(self, s: str) -> bool:
+    #       stack = []
+    #       open_brackets = ['(', '{', '[']
+    #       for letter in s:
+    
     def isValid(self, s: str) -> bool:
         stack = []
         open_brackets = ['(', '{', '[']
-        #close_bracket = [')', '}', '[']
         for letter in s:
-            print(stack)
             if letter in open_brackets:
                 stack.insert(0, letter)
             else:
                 if (len(stack) == 0):
                     return False
                 elif (stack[0] == self.opposite_bracket(letter)):
-                    print(stack[0])
-                    print(self.opposite_bracket(letter))
                     stack.pop(0)
                 else:
                     return False
-        if (len(stack) == 0):
+        if (len(stack) != 0):
             return False
         return True
 
