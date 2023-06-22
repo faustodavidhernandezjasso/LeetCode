77dfd946a584d1237b77872d54f8f5f9123f18da
Fausto David Hernández Jasso <fausto.david.hernandez.jasso@ciencias.unam.mx>
2023-06-22 14:57:34 -0600

    Añadiendo segunda solución

--- DynamicProgramming/N-th_Tribonacci_Number/solution.py
+++ DynamicProgramming/N-th_Tribonacci_Number/solution.py
@@ -1,12 +1,27 @@
 class Solution:
 
     def tribonacci(self, n: int) -> int:
-        T = [0, 1, 1]
-        if (n <= 2):
-            return T[n]
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 1
+        t_n = 0
+        t_n1 = 1
+        t_n2 = 1
         for i in range(3, n + 1):
-            T.append(T[i - 3] + T[i - 2] + T[i - 1])
-        return T[n]
+            result = t_n + t_n1 + t_n2
+            t_n = t_n1
+            t_n1 = t_n2
+            t_n2 = result
+        return result
+        # T = [0, 1, 1]
+        # if (n <= 2):
+        #     return T[n]
+        # for i in range(3, n + 1):
+        #     T.append(T[i - 3] + T[i - 2] + T[i - 1])
+        # return T[n]
     
 
 if __name__ == '__main__':
