98bb2577a1a78a22d07734a863c752affae34a38
Fausto David Hernández Jasso <fausto.david.hernandez.jasso@ciencias.unam.mx>
2023-06-22 14:40:04 -0600

    Añadiendo solución para los números de Fibonacci

---
+++ DynamicProgramming/Fibonacci_Number/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+
+    def fib(self, n: int) -> int:
+        if (n <= 1):
+            return n
+        F = [0, 1]
+        for i in range(2, n + 1):
+            F.append(F[i - 1] + F[i - 2])
+        return F[n]
